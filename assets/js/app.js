cfb2c5602a2ac48f80ca28022aa97a0e78f1aa4d
ins7 <inser7@gmail.com>
2019-12-10 00:54:29 +0300

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../css/app.css';
+import Home from './components/Home';
+    
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
+}
+// ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
